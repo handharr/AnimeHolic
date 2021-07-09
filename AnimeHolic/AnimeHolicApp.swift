fb610f3659f4df6ce0a6d9dcea1b8532b60928a0
Puras Handharmahua <48229056+handharr@users.noreply.github.com>
2021-07-09 09:12:12 +0700

    Initial commit

---
+++ AnimeHolic/AnimeHolicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeHolicApp.swift
+//  AnimeHolic
+//
+//  Created by Puras Handharmahua on 09/07/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimeHolicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
